2f62c10a64be1d1344157a0a9ab95620702dcd46
thiagotmb <tmb0710@gmail.com>
2017-04-29 19:52:56 -0400

    Adding project

---
+++ cloud/main.js
@@ -0,0 +1,5 @@
+//USER AUTHENTICATION MANAGER 
+var path = require('path');
+require(path.join(__dirname, '/vindi-manager'));
+require(path.join(__dirname, '/authentication-manager'));
+require(path.join(__dirname, '/google-spreadsheets-manager.js'));
---
+++ push-notifications/push-notifications-adapter.js
@@ -0,0 +1,47 @@
+
+function getAdapterForEnvironment() {
+    console.log(process.env.NODE_ENV == "production")
+    console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV == "production") {
+        return {
+            ios: [
+                {
+                    pfx: './push-notifications/resources/lab262.ohmybox.store.production.p12', // Dev PFX or P12
+                    bundleId: 'lab262.ohmybox.store.production',
+                    passphrase: '', // optional password to your p12
+                    production: true // Dev
+                }
+            ]
+        }
+    } else if (process.env.NODE_ENV == "test") {
+        return {
+            ios: [
+                {
+                    pfx: './push-notifications/resources/lab262.ohmybox.store.test.p12', // Dev PFX or P12
+                    bundleId: 'lab262.ohmybox.store.test',
+                    passphrase: 'lab26255lab$$$', // optional password to your p12
+                    production: true // Production
+                },
+                {
+                    pfx: './push-notifications/resources/lab262.ohmybox.store.test-dev.p12', // Dev PFX or P12
+                    bundleId: 'lab262.ohmybox.store.test',
+                    passphrase: 'lab26255lab$$$', // optional password to your p12
+                    production: false // Production
+                },
+            ]
+        }
+    } else {
+        return {
+            ios: [
+                {
+                    pfx: './push-notifications/resources/lab262.ohmybox.store.dev.p12', // Dev PFX or P12
+                    bundleId: 'lab262.ohmybox.store.dev',
+                    passphrase: 'lab26255lab$$$', // optional password to your p12
+                    production: false // Dev
+                }
+            ]
+        }
+    }
+}
+console.log(getAdapterForEnvironment())
+module.exports = getAdapterForEnvironment()
